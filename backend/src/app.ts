5c2e690dfdfe67a7845b5db6d5b9c2d451bb088f
mythrhyth <rhythmbhetwal77@gmail.com>
2026-06-18 13:38:56 +0530

    fix(cors): allow frontend deployment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,11 +16,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true
-}));
 
+app.use(cors({
+  origin: true,
+  credentials: true
+}))
 app.use(express.json());
 
 // API Health Check
