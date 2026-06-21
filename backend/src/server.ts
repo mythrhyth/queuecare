4e75d8c4dfca4fc8331f51421783fc21473291c8
mythrhyth <rhythmbhetwal77@gmail.com>
2026-06-21 06:58:47 +0530

    fixing the url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "https://queuecare-neon.vercel.app" || "http://localhost:5173",
+      "https://queuecare-neon.vercel.app",
+      "http://localhost:5173",
       "http://localhost:5174",
       "http://127.0.0.1:5173",
       "http://127.0.0.1:5174"
