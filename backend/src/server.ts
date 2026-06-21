becf08038b5bc14c900d6d007e0d8f859e509163
mythrhyth <rhythmbhetwal77@gmail.com>
2026-06-21 06:53:56 +0530

    added fixes to url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      process.env.FRONTEND_URL || "http://localhost:5173",
+      "https://queuecare-neon.vercel.app" || "http://localhost:5173",
       "http://localhost:5174",
       "http://127.0.0.1:5173",
       "http://127.0.0.1:5174"
